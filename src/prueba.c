8bccdf519f121ecf9567c9519ffa85971b81cfe9
erikgonk <twire01@gmail.com>
2024-11-12 10:19:44 +0100

    update

--- src/prueba.c
+++ src/prueba.c
@@ -1,7 +1,7 @@
 #include "../inc/miniRT.h"
 
-#define WIDTH 2128
-#define HEIGHT 1024
+#define WIDTH 200
+#define HEIGHT 200
 
 static mlx_image_t	*g_img;
 
@@ -12,14 +12,9 @@ void my_keyhook(mlx_key_data_t keydata, void* param)
 	mlx_t* mlx;
 
 	mlx = param;
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT) && keydata.action == MLX_PRESS)
-		x += 0;
-	else if (mlx_is_key_down(mlx, MLX_KEY_LEFT) && keydata.action == MLX_PRESS)
+	if (keydata.key == MLX_KEY_RIGHT)
 	{
-		printf("Hola\n");
-		x -= 0;
+		mlx_close_window(mlx);
 	}
 }
 void	hook(void* param)
@@ -27,12 +22,13 @@ void	hook(void* param)
 	mlx_t* mlx;
 
 	mlx = param;
+	x = 0;
 	if (mlx_is_key_down(mlx, MLX_KEY_P))
 		mlx_delete_image(mlx, g_img);
-	while (++x < 500)
+	while (++x < 25)
 	{
 		int y = 0;
-		while(++y < 500)
+		while(++y < 25)
 			mlx_put_pixel(g_img, x, y, rand() % RAND_MAX);
 	}
 }
@@ -44,7 +40,7 @@ int32_t	main(void)
 	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
 	if (!mlx)
 		exit(EXIT_FAILURE);
-	g_img = mlx_new_image(mlx, 500, 500);
+	g_img = mlx_new_image(mlx, 100, 50);
 	mlx_image_to_window(mlx, g_img, 0, 0);
 	mlx_key_hook(mlx, &my_keyhook, NULL);
 	mlx_loop_hook(mlx, &hook, mlx);
