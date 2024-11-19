503c9acc2ca72e96f3c51d5bfb4dbe58d2fc9ac8
erikgonk <twire01@gmail.com>
2024-11-19 09:46:08 +0100

    [sphere] upate

--- src/sphere.c
+++ src/sphere.c
@@ -6,7 +6,7 @@
 /*   By: erigonza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 12:41:39 by erigonza          #+#    #+#             */
-/*   Updated: 2024/11/18 16:55:31 by erigonza         ###   ########.fr       */
+/*   Updated: 2024/11/19 09:45:23 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,51 +28,6 @@ float sphere_ray_intersect(t_v3 ray_start, t_v3 ray_direction,
         return (-1.0f);								// If h < 0, there’s no hit (miss)
     h = sqrtf(h);									// Calculate the exact distance to the hit point
     return (fmaxf(-b - h, 0.0f));					// Return the closest hit distance
-}
-
-int is_in_shadow(t_sphere *sp, t_v3 point, t_light *light) {
-    t_v3 light_dir = subtract(light->pos, point);
-    float mag = sqrtf(dot(light_dir, light_dir));
-    light_dir = vDefine(light_dir.x / mag, light_dir.y / mag, light_dir.z / mag);
-
-    // Cast a shadow ray from the point to the light source
-    float t = sphere_ray_intersect(point, light_dir, sp->sphere_center, sp->sphere_radius);
-    
-    // If the ray intersects the sphere before reaching the light, it's in shadow
-    return (t > 0.0f && t < mag);
-}
-
-uint32_t	new_light(t_light *l, t_sphere *sp, t_v3 iPoint)
-{
-	float			mag;
-	uint32_t		color; 
-	t_v3			normal;
-	t_v3			dir;	// light dir
-	float			i;		// intensity 
-	
-
-	normal = subtract(iPoint, sp->sphere_center);
-	mag = sqrtf(dot(normal, normal));
-	normal = vDefine(normal.x / mag, normal.y / mag, normal.z / mag);
-
-	// Calculate light direction
-	dir = subtract(l->pos, iPoint);
-	mag = sqrtf(dot(dir, dir));
-	dir = vDefine(dir.x / mag, dir.y / mag, dir.z / mag);
-
-	// Calculate lighting intensity
-	i = fmaxf(dot(normal, dir), 0.0f) * l->br;
-
-	if (is_in_shadow(sp, iPoint, l))
-		i = 0.0f;  // No light if in shadow
-	else
-		i = fmaxf(dot(normal, dir), 0.0f) * l->br;
-	// Apply lighting intensity to color
-	color = sp->color;
-	uint8_t r = fminf(((color >> 16) & 0xFF) * i, 255);
-	uint8_t g = fminf(((color >> 8) & 0xFF) * i, 255);
-	uint8_t b = fminf((color & 0xFF) * i, 255);
-	return ((r << 16) | (g << 8) | b);
 }
 
 void	ft_sphere(t_sphere *sp, t_light *light, mlx_image_t *img)
