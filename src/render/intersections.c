640854de20dafa1d60644fe8988ddb0035c675cf
Samuel Hurtado Marin <shurtado@student.42barcelona.com>
2024-12-10 13:24:00 +0100

    doing cylinder

--- src/render/intersections.c
+++ src/render/intersections.c
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 14:48:44 by shurtado          #+#    #+#             */
-/*   Updated: 2024/12/10 12:34:02 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/12/10 12:55:08 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,42 +42,28 @@ bool	intersect_cylinder(t_ray ray, t_obj *cy, float *t)
 {
 	t_v3		oc;
 	t_quadratic	quad;
-	float		radius;
-	float		l;
-	float		y1, y2;
+	float		y1;
+	float		y2;
 
 	oc = vsubstract(ray.origin, cy->pos);
-	quad.a = dot(ray.direction, ray.direction);
-	quad.b = 2.0f * dot(ray.direction, oc);
-	radius = cy->size / 2.0f;
-	quad.c = dot(oc, oc) - (radius * radius);
-	/*
-	quad.a = pow(ray.direction.x, 2) + pow(ray.direction.z, 2);
-	quad.b = 2 * ray.origin.x * ray.direction.x + 2 * ray.origin.z * ray.direction.z;
-	quad.c = pow(ray.origin.x, 2) + pow(ray.origin.z, 2) - 1;
-	*/
+	quad.a = ray.direction.x * ray.direction.x + ray.direction.z * ray.direction.z;
+	quad.b = 2.0f * (oc.x * ray.direction.x + oc.z * ray.direction.z);
+	quad.c = oc.x * oc.x + oc.z * oc.z - ((cy->size / 2.0f) * (cy->size / 2.0f));
 	init_quadratic(&quad, quad.a, quad.b, quad.c);
 	if (!solve_quadratic(&quad))
-	{
 		return (false);
-	}
 	y1 = ray.origin.y + quad.t1 * ray.direction.y;
 	y2 = ray.origin.y + quad.t2 * ray.direction.y;
-
-	// Verificar si t1 está dentro de los límites del cilindro.
 	if (y1 > -0.5f && y1 < 0.5f)
 	{
-		*t = quad.t1; // Almacenar t1 como la solución válida.
+		*t = quad.t1;
 		return (true);
 	}
-
-	// Verificar si t2 está dentro de los límites del cilindro.
 	if (y2 > -0.5f && y2 < 0.5f)
 	{
-		*t = quad.t2; // Almacenar t2 como la solución válida.
+		*t = quad.t2;
 		return (true);
 	}
-
 	return (false);
 }
 
