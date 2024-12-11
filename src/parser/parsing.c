3a22010fb9d15b7f54e93c6304bfa2ee4ffb6a42
erikgonk <twire01@gmail.com>
2024-12-11 13:41:44 +0100

    [parsing] checking params

--- src/parser/parsing.c
+++ src/parser/parsing.c
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 10:53:01 by erigonza          #+#    #+#             */
-/*   Updated: 2024/12/09 11:34:12 by erigonza         ###   ########.fr       */
+/*   Updated: 2024/12/11 13:40:45 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,16 @@ void	correct_file(char *name)
 		exit(er("error: wrong file name -> ", name));
 	else if (name[i] == '.' && name[i + 1] == 'r' && name[i + 2] == 't')
 		return ;
-	exit (er("error: wrong file name -> ", name));
+	exit(er("error: wrong file name -> ", name));
 }
 
 t_obj	*createObj(t_data *data, char *str, int type)
 {
-	char		*tmp;
-	char		*tmp2 = NULL;
-	t_obj		*obj;
+	char	*tmp;
+	char	*tmp2;
+	t_obj	*obj;
 
+	tmp2 = NULL;
 	obj = newObj(obj);
 	obj->type = type;
 	tmp = floatsParse(obj, str, 2, 0);
@@ -40,7 +41,6 @@ t_obj	*createObj(t_data *data, char *str, int type)
 	if (type == 0) // sp
 	{
 		obj->size = ft_atof(tmp, 0);
-		// here skips  number
 		tmp2 = ft_substr(tmp, sumParse(tmp, 0, 2, 0), ft_strlen(tmp));
 	}
 	else if (type == 2) // cy
@@ -61,8 +61,75 @@ t_obj	*createObj(t_data *data, char *str, int type)
 	free(tmp);
 	if (tmp2)
 		free(tmp2);
-	obj->type = (char)type;
+	obj->type = type;
 	return (obj);
+}
+
+void	checkParamsACL(t_aLight *aL, t_sLight *sL, t_cam *cam)
+{
+	printf("br -> %d\n", aL->rgb.r);
+	if ((!aL && !sL) || !cam)
+		exit(er("error: ACL left", NULL));
+	else if (aL)
+		if (aL->br < 0)
+			exit(er("error: brightness < 0", NULL));
+		else if (aL->rgb.r < 0 || aL->rgb.r > 255)
+			exit(er("error: aL rgb.r <0 / >255", NULL));
+		else if (aL->rgb.g < 0 || aL->rgb.g > 255)
+			exit(er("error: aL rgb.g <0 / >255", NULL));
+		else if (aL->rgb.b < 0 || aL->rgb.b > 255)
+			exit(er("error: aL rgb.b <0/>255", NULL));
+	while (sL)
+	{
+		if (sL->br < 0)
+			exit(er("error: ACL left", NULL));
+		else if (sL->rgb.r < 0 || sL->rgb.r > 255)
+			exit(er("error: sL rgb.r <0 / >255", NULL));
+		else if (sL->rgb.g < 0 || sL->rgb.g > 255)
+			exit(er("error: sL rgb.g <0 / >255", NULL));
+		else if (sL->rgb.b < 0 || sL->rgb.b > 255)
+			exit(er("error: sL rgb.b <0/>255", NULL));
+		sL = sL->next;
+	}
+	if (cam->axis.x < 0 || cam->axis.x > 1)
+		exit(er("error: cam axis.x <0 / <1", NULL));
+	else if (cam->axis.y < 0 || cam->axis.y > 1)
+		exit(er("error: cam axis.y <0 / <1", NULL));
+	else if (cam->axis.z < 0 || cam->axis.z > 1)
+		exit(er("error: cam axis.z <0 / <1", NULL));
+}
+
+void	checkParams(t_data *data)
+{
+	t_obj	*obj;
+
+	obj = data->obj;
+	if (!obj)
+		exit(er("erro: no obj detected", NULL));
+	while (obj)
+	{
+		if (obj->rgb.r < 0 || obj->rgb.r > 255)
+			exit(er("error: rgb.r <0 / >255", NULL));
+		else if (obj->rgb.g < 0 || obj->rgb.g > 255)
+			exit(er("error: rgb.g <0 / >255", NULL));
+		else if (obj->rgb.b < 0 || obj->rgb.b > 255)
+			exit(er("error: rgb.b <0/>255", NULL));
+		else if (obj->type != PL && obj->size <= 0)
+			exit(er("error: sp/cy size <= 0", NULL));
+		else if (obj->type != SP)
+		{
+			if (obj->axis.x < 0 || obj->axis.x > 1)
+				exit(er("error: pl/cy axis.x <0 / <1", NULL));
+			else if (obj->axis.y < 0 || obj->axis.y > 1)
+				exit(er("error: pl/cy axis.y <0 / <1", NULL));
+			else if (obj->axis.z < 0 || obj->axis.z > 1)
+				exit(er("error: pl/cy axis.z <0 / <1", NULL));
+			else if (obj->type == CY && obj->height < 0)
+				exit(er("error: cy height < 0", NULL));
+		}
+		obj = obj->next;
+	}
+	checkParamsACL(data->sLight, data->sLight, data->cam);
 }
 
 void	parse(t_data *data, char **av, int fd)
@@ -83,31 +150,13 @@ void	parse(t_data *data, char **av, int fd)
 			continue ;
 		type = checkObj(data, str);
 		if (type > 5)
-			exit (er("error: wrong map: obj type", str));
+			exit(er("error: wrong map: obj type", str));
 		if (type <= 2 && ft_isspace(str[2]))
 			objadd_back(&data->obj, createObj(data, str, type));
 		createALight(data, str, type);
 		createCam(data, str, type);
 		createSLight(data, str, type);
 	}
+	// checkParams(data);
 	close(fd);
 }
-	// data->obj->ray_start = vdefine(0.0, 0.0, 0.0);		// Camera position (where our rays start from)
-	// data->obj->sphere_radius = 1.4;						// Radius (size) of the sphere
- //    data->obj->sphere_center = vdefine(
- //        atof(av[1]),
- //        atof(av[2]),
- //        atof(av[3]));
-
- //    data->obj->sphere_radius = atof(av[4]);  // Sphere radius
- //    data->obj->color = ((atoi(av[5]) << 16) | (atoi(av[6]) << 8) | atoi(av[7]));
-
- //    // Initialize light source
- //    data->sLight->pos = vdefine(
- //        atof(av[8]),  // X
- //        atof(av[9]),  // Y
- //        atof(av[10])  // Z
- //    );
- //    data->sLight->br = atof(av[11]);  // Light brightness ratio
- //    if (data->sLight->br < 0.0f || data->sLight->br > 1.0f)
- //        er("error: %s: brightness must be in range [0.0, 1.0]", av[11]);
