e878660720f1cbd7991e0494284db3a1c4ce50e7
Samuel Hurtado <lnx.wifi@gmail.com>
2024-12-14 12:47:05 +0100

    [submenu.c] console improvements

--- src/console/submenu.c
+++ src/console/submenu.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   submenu.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
+/*   By: shurtado <shurtado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:57:09 by shurtado          #+#    #+#             */
-/*   Updated: 2024/12/11 10:22:38 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/12/14 12:43:34 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,77 @@
 
 void	manage_alight(keys_t key, t_data *data)
 {
+	static float quantity;
+	
+	if (quantity < 0.01f)
+		quantity = ALIGHTPLUS;
 	if (key == MLX_KEY_UP && data->aLight->br < ALIGHTMAX)
-		data->aLight->br = fminf(data->aLight->br + ALIGHTPLUS, ALIGHTMAX);
+		data->aLight->br = fminf(data->aLight->br + quantity, ALIGHTMAX);
 	else if (key == MLX_KEY_DOWN && data->aLight->br > ALIGHTMIN)
-		data->aLight->br = fmaxf(data->aLight->br - ALIGHTPLUS, ALIGHTMIN);
+		data->aLight->br = fmaxf(data->aLight->br - quantity, ALIGHTMIN);
+	else if (key == MLX_KEY_MINUS || key == MLX_KEY_KP_SUBTRACT)
+		quantity = fmaxf(quantity - 0.01f, 0.01);
+	else if (key == MLX_KEY_KP_ADD || key == MLX_KEY_EQUAL)
+		quantity += 0.01f;
 	print_alight_menu(data);
+	printf("\nincrementos = %f\n", quantity);
 }
 
 void	manage_cam(keys_t key, t_data *data)
 {
-	static bool	mode;
+	static bool		mode;
+	static float	posq;
+	static float	axisq;
 
+	if (posq < 1.0f)
+		posq = CAMPLUS;
+	if (axisq < 0.01f)
+		axisq = CAMAXISP;
 	if (key == MLX_KEY_SPACE)
 		mode = !mode;
 	if (!mode)
 	{
 		if (key == MLX_KEY_UP)
-			data->cam->pos.y += CAMPLUS;
+			data->cam->pos.y += posq;
 		else if (key == MLX_KEY_DOWN)
-			data->cam->pos.y -= CAMPLUS;
+			data->cam->pos.y -= posq;
 		else if (key == MLX_KEY_LEFT)
-			data->cam->pos.x -= CAMPLUS;
+			data->cam->pos.x -= posq;
 		else if (key == MLX_KEY_RIGHT)
-			data->cam->pos.x += CAMPLUS;
+			data->cam->pos.x += posq;
 		else if (key == MLX_KEY_Z)
-			data->cam->pos.z -= CAMPLUS;
+			data->cam->pos.z -= posq;
 		else if (key == MLX_KEY_X)
-			data->cam->pos.z += CAMPLUS;
+			data->cam->pos.z += posq;
+		else if (key == MLX_KEY_MINUS || key == MLX_KEY_KP_SUBTRACT)
+			posq = fmaxf(1.0f, posq -1);
+		else if (key == MLX_KEY_KP_ADD || key == MLX_KEY_EQUAL)
+			posq = fminf(100, posq + 1);
+
 	}
 	else
 	{
 		if (key == MLX_KEY_UP)
-			data->cam->axis.y = fminf(data->cam->axis.y + CAMAXISP, CAMAXISMAX);
+			data->cam->axis.y = fminf(data->cam->axis.y + axisq, CAMAXISMAX);
 		else if (key == MLX_KEY_DOWN)
-			data->cam->axis.y = fmaxf(data->cam->axis.y - CAMAXISP, CAMAXISMIN);
+			data->cam->axis.y = fmaxf(data->cam->axis.y - axisq, CAMAXISMIN);
 		else if (key == MLX_KEY_LEFT)
-			data->cam->axis.x = fmaxf(data->cam->axis.x - CAMAXISP, CAMAXISMIN);
+			data->cam->axis.x = fmaxf(data->cam->axis.x - axisq, CAMAXISMIN);
 		else if (key == MLX_KEY_RIGHT)
-			data->cam->axis.x = fminf(data->cam->axis.x + CAMAXISP, CAMAXISMAX);
+			data->cam->axis.x = fminf(data->cam->axis.x + axisq, CAMAXISMAX);
 		else if (key == MLX_KEY_Z)
-			data->cam->axis.z = fmaxf(data->cam->axis.z - CAMAXISP, CAMAXISMIN);
+			data->cam->axis.z = fmaxf(data->cam->axis.z - axisq, CAMAXISMIN);
 		else if (key == MLX_KEY_X)
-			data->cam->axis.z = fminf(data->cam->axis.z + CAMAXISP, CAMAXISMAX);
+			data->cam->axis.z = fminf(data->cam->axis.z + axisq, CAMAXISMAX);
+		else if (key == MLX_KEY_MINUS || key == MLX_KEY_KP_SUBTRACT)
+			axisq = fmaxf(0.01f, axisq -0.01f);
+		else if (key == MLX_KEY_KP_ADD || key == MLX_KEY_EQUAL)
+			axisq = fminf(1.0f, axisq + 0.01f);
 	}
 	print_cam_menu(data);
+	print_alight_menu(data);
+	printf("\nincrementos pos= %f\n", posq);
+	printf("incrementos axis= %f\n", axisq);
 }
 
 void	manage_spot(keys_t key, t_data *data)
@@ -99,7 +126,13 @@ void	manage_obj(keys_t key, t_data *data)
 {
 	static bool			mode;
 	static t_obj		*obj;
+	static float		posq;
+	static float		axisq;
 
+	if (posq < 1.0f)
+		posq = OBJPLUS;
+	if (axisq < 0.001f)
+		axisq = OBJAXISP;
 	if (!obj)
 		obj = data->obj;
 	if (key == MLX_KEY_N)
@@ -114,34 +147,44 @@ void	manage_obj(keys_t key, t_data *data)
 	if (!mode)
 	{
 		if (key == MLX_KEY_UP)
-			obj->pos.y += OBJPLUS;
+			obj->pos.y += posq;
 		else if (key == MLX_KEY_DOWN)
-			obj->pos.y -= OBJPLUS;
+			obj->pos.y -= posq;
 		else if (key == MLX_KEY_LEFT)
-			obj->pos.x -= OBJPLUS;
+			obj->pos.x -= posq;
 		else if (key == MLX_KEY_RIGHT)
-			obj->pos.x += OBJPLUS;
+			obj->pos.x += posq;
 		else if (key == MLX_KEY_Z)
-			obj->pos.z -= OBJPLUS;
+			obj->pos.z -= posq;
 		else if (key == MLX_KEY_X)
-			obj->pos.z += OBJPLUS;
+			obj->pos.z += posq;
+		else if (key == MLX_KEY_MINUS || key == MLX_KEY_KP_SUBTRACT)
+			posq = fmaxf(1.0f, posq -1);
+		else if (key == MLX_KEY_KP_ADD || key == MLX_KEY_EQUAL)
+			posq = fminf(100, posq + 1);
 	}
 	else
 	{
 		if (key == MLX_KEY_UP)
-			obj->axis.y = fminf(obj->axis.y + OBJAXISP, OBJAXISMAX);
+			obj->axis.y = fminf(obj->axis.y + axisq, OBJAXISMAX);
 		else if (key == MLX_KEY_DOWN)
-			obj->axis.y = fmaxf(obj->axis.y - OBJAXISP, OBJAXISMIN);
+			obj->axis.y = fmaxf(obj->axis.y - axisq, OBJAXISMIN);
 		else if (key == MLX_KEY_LEFT)
-			obj->axis.x = fmaxf(obj->axis.x - OBJAXISP, OBJAXISMIN);
+			obj->axis.x = fmaxf(obj->axis.x - axisq, OBJAXISMIN);
 		else if (key == MLX_KEY_RIGHT)
-			obj->axis.x = fminf(obj->axis.x + OBJAXISP, OBJAXISMAX);
+			obj->axis.x = fminf(obj->axis.x + axisq, OBJAXISMAX);
 		else if (key == MLX_KEY_Z)
-			obj->axis.z = fmaxf(obj->axis.z - OBJAXISP, OBJAXISMIN);
+			obj->axis.z = fmaxf(obj->axis.z - axisq, OBJAXISMIN);
 		else if (key == MLX_KEY_X)
-			obj->axis.z = fminf(obj->axis.z + OBJAXISP, OBJAXISMAX);
+			obj->axis.z = fminf(obj->axis.z + axisq, OBJAXISMAX);
+		else if (key == MLX_KEY_MINUS || key == MLX_KEY_KP_SUBTRACT)
+			axisq = fmaxf(0.01f, axisq -0.01f);
+		else if (key == MLX_KEY_KP_ADD || key == MLX_KEY_EQUAL)
+			axisq = fminf(1.0f, axisq + 0.01f);
 	}
 	print_obj_menu(data);
+	printf("\nincrementos pos= %f\n", posq);
+	printf("incrementos axis= %f\n", axisq);
 }
 
 void	manage_submenu(keys_t key, int option, t_data *data)
