37c13580e5d27fad5d91d9c53fea06a790282617
Samuel Hurtado Marin <shurtado@student.42barcelona.com>
2024-12-17 14:54:06 +0100

    Mandatory Done

--- src/parser/parseObj.c
+++ src/parser/parseObj.c
@@ -3,14 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   parseObj.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shurtado <shurtado@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 17:02:22 by erigonza          #+#    #+#             */
-/*   Updated: 2024/12/17 14:46:47 by erigonza         ###   ########.fr       */
+/*   Updated: 2024/12/17 14:52:11 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+void	create_obj_normi(t_obj *obj, char **tmp, char **tmp2)
+{
+	if (obj->type == 0)
+	{
+		obj->size = ft_atof(*tmp, 0);
+		*tmp2 = ft_substr(*tmp, sum_parse(*tmp, 0, 2, 0), ft_strlen(*tmp));
+	}
+	else if (obj->type == 2)
+	{
+		obj->size = ft_atof(*tmp2, 0);
+		obj->i = sum_parse(*tmp2, 0, 2, 0);
+		obj->height = ft_atof(*tmp2, obj->i);
+		obj->i = sum_parse(*tmp2, obj->i, 2, 0);
+		free(*tmp);
+		*tmp = ft_substr(*tmp2, obj->i, ft_strlen(*tmp2));
+	}
+}
 
 t_obj	*create_obj(t_data *data, char *str, int type)
 {
@@ -24,20 +42,7 @@ t_obj	*create_obj(t_data *data, char *str, int type)
 	tmp = floats_parse(obj, str, 2, 0);
 	if (obj->type != 0)
 		tmp2 = floats_parse(obj, tmp, 0, 1);
-	if (obj->type == 0)
-	{
-		obj->size = ft_atof(tmp, 0);
-		tmp2 = ft_substr(tmp, sum_parse(tmp, 0, 2, 0), ft_strlen(tmp));
-	}
-	else if (obj->type == 2)
-	{
-		obj->size = ft_atof(tmp2, 0);
-		obj->i = sum_parse(tmp2, 0, 2, 0);
-		obj->height = ft_atof(tmp2, obj->i);
-		obj->i = sum_parse(tmp2, obj->i, 2, 0);
-		free(tmp);
-		tmp = ft_substr(tmp2, obj->i, ft_strlen(tmp2));
-	}
+	create_obj_normi(obj, &tmp, &tmp2);
 	if (obj->type != 2)
 	{
 		check_end(obj, tmp2);
