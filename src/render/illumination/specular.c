d19bb0c4b7347fe688a92562735323b238c71a95
Samuel Hurtado Marin <shurtado@student.42barcelona.com>
2024-12-21 13:26:26 +0100

    adjusting material roughness

--- src/render/illumination/specular.c
+++ src/render/illumination/specular.c
@@ -6,7 +6,7 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 09:41:20 by shurtado          #+#    #+#             */
-/*   Updated: 2024/12/20 11:34:03 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/12/21 13:25:43 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,33 @@ void	specular_light(t_rgb *color, t_slight *slight, t_ray *ray, t_ray *s_ray)
 {
 	float		intensity;
 	t_v3		reflection;
+	int			roughness;
+	float		color_roughness;
+	int			r;
+	int			g;
+	int			b;
 
+	roughness = 64;
+	if (roughness < 8)
+	{
+		color_roughness = 50;
+		r = 255;
+		g = 255;
+		b = 255;
+	}
+	else if (roughness < 64)
+		color_roughness = 150;
+	else
+		color_roughness = 255;
+	if (roughness >= 8)
+	{
+		r = (color->r + color_roughness) / 2;
+		g = (color->g + color_roughness) / 2;
+		b = (color->b + color_roughness) / 2;
+	}
 	reflection = vrefl(vneg(s_ray->direction), ray->normal);
-	intensity = pow(fmax(dot(reflection, ray->i_direction), 0.0f), SOLID_S);
-	color->r = fminf(color->r + slight->rgb.r * intensity, 255.0f);
-	color->g = fminf(color->g + slight->rgb.g * intensity, 255.0f);
-	color->b = fminf(color->b + slight->rgb.b * intensity, 255.0f);
+	intensity = pow(fmax(dot(reflection, ray->i_direction), 0.0f), roughness);
+	color->r = fminf(color->r + slight->rgb.r * intensity, r);
+	color->g = fminf(color->g + slight->rgb.g * intensity, g);
+	color->b = fminf(color->b + slight->rgb.b * intensity, b);
 }
